0f94ffd1be86b865147f0dcb6ece0f6f8ccc3d4e
Joshua Johnson <joshuaj1397@gmail.com>
2017-08-09 22:23:00 -0500

    Overhauled frontend and backend to use Node.js, Express, and Jade

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'TTU ACM' });
+});
+
+/* GET calendar page. */
+router.get('calendar', function(req, res, next) {
+  res.render('calendar', { title: 'TTU ACM' });
+});
+
+/* GET team page. */
+router.get('/team', function(req, res, next) {
+  res.render('team', { title: 'TTU ACM' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'TTU ACM' });
+});
+
+//TODO: Login functionality
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users/detail', function(req, res, next) {
+    res.send('detail');
+});
+
+module.exports = router;
