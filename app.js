d7681e5eac3b8894081d0923a6cfcbff8f6635da
Joshua Johnson <joshuaj1397@gmail.com>
2017-09-01 21:07:38 -0500

    Finished login/registration system

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var flash = require('connect-flash');
+var session = require('express-session');
+var configDB = require('./config/database.js');
+var mongoose = require('mongoose');
 
-var ArticleProvider = require('./public/javascripts/articleprovider').ArticleProvider;
 var index = require('./routes/index');
 
 var app = express();
+
+mongoose.connect(configDB.url)
+require('./config/passport')(passport);
+
+// Passport setup
+app.use(session({secret: '***REMOVED***'})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +35,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+
+// Routes
+require('./routes/index')(app, passport);
+//app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
