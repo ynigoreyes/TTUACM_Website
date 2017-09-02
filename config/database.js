ff14b19e6a083147f14cc954572df41e04527855
Joshua Johnson <joshuaj1397@gmail.com>
2017-09-01 21:07:38 -0500

    Finished login/registration system

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url': 'mongodb://joshuaj1397:Blind1572@ttuacm-shard-00-00-vssjo.mongodb.net:27017,ttuacm-shard-00-01-vssjo.mongodb.net:27017,ttuacm-shard-00-02-vssjo.mongodb.net:27017/test?ssl=true&replicaSet=TTUACM-shard-0&authSource=admin'
+}
