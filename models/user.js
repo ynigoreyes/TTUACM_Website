3e4a064824944f6cec6b382ee9946a49ef923353
Joshua Johnson <joshuaj1397@gmail.com>
2017-09-04 00:09:55 -0500

    Finished password reset functionality; partially finished profile page

--- models/user.js
+++ models/user.js
@@ -1,20 +1,50 @@
+/*
+* NOTE:@param SALT_FACTOR is different in some functions so that salted/hashed
+*     passwords can not be cross referenced in case of a database failure.
+*/
+
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = mongoose.Schema({
-    local               : {
-        email           : String,
-        password        : String,
-        firstName       : String,
-        lastName        : String,
-        classification  : String
+    local                   : {
+        email               : {type: String, required: true, unique: true},
+        password            : {type: String, required: true},
+        firstName           : String,
+        lastName            : String,
+        classification      : String,
+        resetPasswordToken  : String,
+        resetPasswordExpires: Date
     }
 });
 
+
+// This function is saltier than the Salty Spitoon
+userSchema.pre('save', function(next) {
+    var user = this;
+    var SALT_FACTOR = 5;
+
+    if (!user.isModified('local.password')) return next();
+
+    // Make some salt
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // Sprinkle the salt
+        bcrypt.hash(user.local.password, salt, null, function(err, hash) {
+            if (err) return next();
+            user.local.password = hash;
+            next();
+        });
+    });
+});
+
+// Make some more salt, but throw some sand in this batch
 userSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
+// Separate the salt and sand
 userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
