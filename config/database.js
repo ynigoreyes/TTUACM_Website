5160cbe0f42f5c7b458a67b4ce6e0f6c6e1d239c
Joshua Johnson <joshuaj1397@gmail.com>
2017-09-04 10:00:41 -0500

    Changed database account to acmtexastech@gmail.com

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url': 'mongodb://joshuaj1397:Blind1572@ttuacm-shard-00-00-vssjo.mongodb.net:27017,ttuacm-shard-00-01-vssjo.mongodb.net:27017,ttuacm-shard-00-02-vssjo.mongodb.net:27017/test?ssl=true&replicaSet=TTUACM-shard-0&authSource=admin'
+    'url': 'mongodb://acmtexastech:T0rt!11as@ttuacm-shard-00-00-tn7s1.mongodb.net:27017,ttuacm-shard-00-01-tn7s1.mongodb.net:27017,ttuacm-shard-00-02-tn7s1.mongodb.net:27017/test?ssl=true&replicaSet=ttuacm-shard-0&authSource=admin'
 }
