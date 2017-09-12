cd516156850cb1641f75eed4d70e9116a07aab4a
Joshua Johnson <joshuaj1397@gmail.com>
2017-09-11 20:25:22 -0500

    Impletemented confirm email option and dues field in user model.

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user');
-var nodemailer = require('nodemailer');
 var crypto = require('crypto');
+var nodemailer = require('nodemailer');
 var async = require('async');
 
 module.exports = function(passport) {
@@ -26,6 +26,7 @@ module.exports = function(passport) {
             if (err) return done(err);
             if (!user) return done(null, false, req.flash('loginMessage', 'Invalid Username.'));
             if (!user.validPassword(password)) return done(null, false, req.flash('loginMessage', 'Invalid Password.'));
+            if (!user.verified) return done(null, false, req.flash('loginMessage', 'Please confirm your email.'));
             return done(null, user);
         });
     }));
@@ -45,15 +46,48 @@ module.exports = function(passport) {
                     return done(null, false, req.flash('signupMessage', 'Password and confirm password must match.'))
                 } else {
                     var newUser = new User();
+                    async.waterfall([
+
+                        function(done) {
+                            var token = crypto.createHmac('sha256', email).digest('hex');
+                            newUser.local.confirmEmailToken = token;
+                            done(err, token);
+                        },
+
+                        function(token, done) {
+                            var smtpTransport = nodemailer.createTransport({
+                                service: 'Gmail',
+                                auth: {
+                                    //TODO: Use OAuth2
+                                    user: 'acmtexastech@gmail.com',
+                                    pass: 'w1nnersallofus'
+                                }
+                            });
+                            var mailOptions = {
+                                to: email,
+                                from: 'Texas Tech ACM',
+                                subject: 'Confirm Account for TTU ACM',
+                                text: 'You are receiving this because you (or someone else) have signed up for an account with Texas Tech Association of Computing Machinery.\n\n' +
+                                    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                                    req.protocol + '://' + req.headers.host + '/confirm/' + token + '\n\n'
+                            };
+                            smtpTransport.sendMail(mailOptions, function(err) {
+                                done(err);
+                            });
+                        }
+                    ], function(err) {
+                        if (err) return next(err);
+                    });
                     newUser.local.firstName = req.body.firstName;
                     newUser.local.lastName = req.body.lastName;
                     newUser.local.email = email;
                     newUser.local.password = password;
                     newUser.local.classification = req.body.classification;
+                    newUser.local.hasPaidDues = false;
                     newUser.save(function(err) {
                         if (err)
                             throw err;
-                        return done(null, newUser);
+                        return done(null, newUser, req.flash('loginMessage', 'We have sent an email to ' + email + ' to confirm your account.'));
                     });
                 }
             });
