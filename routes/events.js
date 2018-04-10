10c075a8cc0e3337ecf92d3905e54ea7102832e3
ynigoreyes <ynigoreyes@gmail.com>
2018-04-10 12:02:22 -0500

    added the events route

---
+++ routes/events.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+
+// Controller
+const EventsController = require('../controllers/event_c');
+
+router.get('/', EventsController.googleAuthenticate);
