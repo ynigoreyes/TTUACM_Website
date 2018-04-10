0473800ad8dad98d4236c64a71e3713e0e18c85c
ynigoreyes <ynigoreyes@gmail.com>
2018-04-10 15:13:49 -0500

    removed database creds

--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: 'mongodb://acmtexastech:***REMOVED***@ttuacm-shard-00-00-tn7s1.mongodb.net:27017,ttuacm-shard-00-01-tn7s1.mongodb.net:27017,ttuacm-shard-00-02-tn7s1.mongodb.net:27017/test?ssl=true&replicaSet=ttuacm-shard-0&authSource=admin'
-}
