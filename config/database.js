06f36195fc676f0e2533710468462816d29874e1
ynigoreyes <ynigoreyes@gmail.com>
2018-04-10 11:47:08 -0500

    New Structure

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url': 'mongodb://acmtexastech:T0rt!11as@ttuacm-shard-00-00-tn7s1.mongodb.net:27017,ttuacm-shard-00-01-tn7s1.mongodb.net:27017,ttuacm-shard-00-02-tn7s1.mongodb.net:27017/test?ssl=true&replicaSet=ttuacm-shard-0&authSource=admin'
+  'url': 'mongodb://acmtexastech:***REMOVED***@ttuacm-shard-00-00-tn7s1.mongodb.net:27017,ttuacm-shard-00-01-tn7s1.mongodb.net:27017,ttuacm-shard-00-02-tn7s1.mongodb.net:27017/test?ssl=true&replicaSet=ttuacm-shard-0&authSource=admin'
 }
