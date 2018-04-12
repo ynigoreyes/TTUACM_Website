dd9b10034322203edb3c5f6b2858efe639abaef9
ynigoreyes <ynigoreyes@gmail.com>
2018-04-12 16:20:28 -0500

    Added the modal and removed the Contact Tab

--- site/src/app/components/footer/footer.component.ts
+++ site/src/app/components/footer/footer.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { ContactModalComponent } from '../contact-modal/contact-modal.component';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ContactService } from '../../services/contact.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    private contactService: ContactService,
+    private flashMessage: FlashMessagesService
+  ) { }
+  private name:     String = null;
+  private email:    String = null;
+  private message:  String = null;
 
-  ngOnInit() {
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ContactModalComponent, {
+      width: '700px',
+      data: {
+        name: this.name,
+        email: this.email,
+        message: this.message
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+
+      // If the user has pressed the Confirm button
+      if (result) {
+        const message = {
+          name: result.name,
+          email: result.email,
+          message: result.message
+        };
+        this.contactService.sendEmail(message).subscribe(data => {
+          if (data.success) {
+            alert('Success Sending Message');
+          } else {
+            alert('Error Sending Message');
+          }
+        });
+      }
+
+    });
   }
-
 }
