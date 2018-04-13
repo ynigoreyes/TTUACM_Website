3c4a03893e3c4db728a89d35d491c63a67d9e583
ynigoreyes <ynigoreyes@gmail.com>
2018-04-13 16:42:22 -0500

    Finished the design for the Google Calendar

--- site/src/app/components/events/events.component.ts
+++ site/src/app/components/events/events.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.css']
 })
-export class EventsComponent implements OnInit {
+export class EventsComponent {
+  events: any;
+  displayedEvents: any;
 
-  constructor() { }
+  lengthOfEvents: number;
+  minNumberEvent: number;
+  maxNumberEvent: number;
+  changeAmount = 10;
 
-  ngOnInit() {
+  constructor(private http: Http) {
+    this.http.get('http://localhost:80/events/get-events').subscribe((results) => {
+
+    this.minNumberEvent = 0;
+    this.maxNumberEvent = 10;
+    this.events = results.json();
+    this.lengthOfEvents = this.events.data.length;
+
+    this.displayedEvents = this.events.data.slice(this.minNumberEvent, this.maxNumberEvent);
+    });
+  }
+
+  getNextEvents() {
+    if (this.maxNumberEvent < this.lengthOfEvents) {
+      this.minNumberEvent += this.changeAmount;
+      this.maxNumberEvent += this.changeAmount;
+    } else {
+      this.minNumberEvent = 0;
+      this.maxNumberEvent = 10;
+    }
+
+    this.displayedEvents = this.events.data.slice(this.minNumberEvent, this.maxNumberEvent);
+  }
+
+  getPrevEvents() {
+    if (this.minNumberEvent >= this.changeAmount) {
+      this.minNumberEvent -= this.changeAmount;
+      this.maxNumberEvent -= this.changeAmount;
+    } else {
+      this.minNumberEvent = this.lengthOfEvents - this.changeAmount;
+      this.maxNumberEvent = this.lengthOfEvents;
+    }
+    this.displayedEvents = this.events.data.slice(this.minNumberEvent, this.maxNumberEvent);
   }
 
 }
