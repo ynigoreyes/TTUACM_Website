ddf539d980b36ca3597444b27d3df521e3e6d8f8
Miggy Reyes <ynigoreyes@gmail.com>
2018-05-17 03:23:24 -0500

    Commenting and organization
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch convert
     Your branch is ahead of 'origin/convert' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   config/passport.js
    	modified:   controllers/auth_c.js
    	modified:   routes/auth.js
    	modified:   routes/users.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,12 @@ const AuthCtrl = require('../controllers/auth_c');
 authRouter.get('/google', passport.authenticate('google', {scope: ['profile']}));
 
 /* Callback URL for Google */
-authRouter.get('/google/redirect', passport.authenticate('google'), AuthCtrl.google);
+authRouter.get('/google/redirect', passport.authenticate('google'), AuthCtrl.oauth2);
+
+/* GETS the GitHub Login Screen */
+authRouter.get('/github', passport.authenticate('github'));
+
+/* Callback URL for GitHub */
+authRouter.get('/github/redirect', passport.authenticate('github'), AuthCtrl.oauth2);
 
 module.exports = authRouter;
