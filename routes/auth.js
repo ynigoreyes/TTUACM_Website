791328dde8cccf8d0d5828341346c5acaa49c7ef
Miggy Reyes <ynigoreyes@gmail.com>
2018-05-18 13:38:54 -0500

    Google and GitHub Users now have emails

--- routes/auth.js
+++ routes/auth.js
@@ -5,13 +5,21 @@ const authRouter = express.Router();
 const AuthCtrl = require('../controllers/auth_c');
 
 /* GETS the Google Login Screen */
-authRouter.get('/google', passport.authenticate('google', {scope: ['profile']}));
+authRouter.get('/google', passport.authenticate('google', {
+  scope: [
+    'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email']
+}));
 
 /* Callback URL for Google */
 authRouter.get('/google/redirect', passport.authenticate('google'), AuthCtrl.oauth2);
 
 /* GETS the GitHub Login Screen */
-authRouter.get('/github', passport.authenticate('github'));
+authRouter.get('/github', passport.authenticate('github', {
+  scope: [
+    'user'
+  ]
+}));
 
 /* Callback URL for GitHub */
 authRouter.get('/github/redirect', passport.authenticate('github'), AuthCtrl.oauth2);
