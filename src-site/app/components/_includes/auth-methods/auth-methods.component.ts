28cf8a64a77fc906e698f6625dca52cae340bd4f
Miggy Reyes <ynigoreyes@gmail.com>
2018-05-18 01:43:19 -0500

    github auth completed

---
+++ src-site/app/components/_includes/auth-methods/auth-methods.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-methods',
+  templateUrl: './auth-methods.component.html',
+  styleUrls: ['./auth-methods.component.scss']
+})
+export class AuthMethodsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
