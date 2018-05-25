221a24098a72afc74ba6873d41a2f2c431ffe762
Miggy Reyes <ynigoreyes@gmail.com>
2018-05-25 00:41:30 -0500

    finished rewriting forgot password and reset functionality

--- src-site/app/components/forgot/prompt/prompt.component.ts
+++ src-site/app/components/forgot/prompt/prompt.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-prompt',
   templateUrl: './prompt.component.html',
   styleUrls: ['./prompt.component.scss']
 })
-export class PromptComponent {
+export class PromptComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private snackbar: MatSnackBar
   ) { }
-  forgotForm = new FormGroup({
-    email: new FormControl('', { validators: [Validators.required, Validators.email] })
+
+  public forgotForm = new FormGroup({
+    email: new FormControl('', { validators: [
+      Validators.required, Validators.email] })
   });
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .subscribe(params => {
+        if (params['tokenError'] === 'true') {
+          setTimeout(() => {
+            this.snackbar.open('Error verifying user request',
+            'Close', {duration: 3000});
+          }, 200);
+        }
+      });
+  }
+
 
   public attemptForget(post: FormGroup) {
     const currentEmail = post['email'].trim();
