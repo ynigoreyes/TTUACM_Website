18f8a2342499f00464f6fd6aa13d5a6b7e866bcf
Miggy Reyes <ynigoreyes@gmail.com>
2018-05-27 03:56:11 -0500

    Events page functionality done

--- src-site/app/services/events.service.ts
+++ src-site/app/services/events.service.ts
@@ -3,31 +3,13 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class EventsService {
+  private PORT: string = '8080';
 
   constructor(private http: HttpClient) { }
-  getEventsEP: string = 'http://localhost:80/events/get-events';
-
-  // TODO: Make these routes and see if it would be better to just
-  // Put some logic in the routes instead of making endpoints for all
-  // of the different buttons
-  attendingEventsEP: string = 'http://localhost:80/events/attending-events';
-  maybeEventsEP: string = 'http://localhost:80/events/maybe-attending-events';
-  notAttendingEventsEP: string = 'http://localhost:80/events/not-attending-events';
+  getEventsEP: string = `http://localhost:${this.PORT}/events/`;
 
   public getEvents () {
     return this.http.get(this.getEventsEP);
-  }
-
-  public attendingEvent(user, event) {
-    console.log('Attending');
-  }
-
-  public maybeEvent(user, event) {
-    console.log('Maybe Attending');
-  }
-
-  public notAttendingEvent(user, event) {
-    console.log('Not Attending');
   }
 
 }
