a473804b87797b0b7d0f4a953ee9ac1a1ca5a96e
Miggy Reyes <ynigoreyes@gmail.com>
2018-06-05 13:42:24 -0500

    facebook auth working

--- routes/auth.js
+++ routes/auth.js
@@ -5,27 +5,45 @@ const authRouter = express.Router();
 const AuthCtrl = require('../controllers/auth_c');
 
 /* GETS the Google Login Screen */
-authRouter.get('/google', passport.authenticate('google', {
-  scope: [
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/userinfo.email']
-}));
+authRouter.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ]
+  })
+);
 
 /* Callback URL for Google */
 authRouter.get('/google/redirect', passport.authenticate('google'), AuthCtrl.oauth2);
 
-authRouter.get('/google/calendar', (req, res) => {
-  console.log(res, req);
-});
-
 /* GETS the GitHub Login Screen */
-authRouter.get('/github', passport.authenticate('github', {
-  scope: [
-    'user'
-  ]
-}));
+authRouter.get(
+  '/github',
+  passport.authenticate('github', {
+    scope: ['user']
+  })
+);
 
 /* Callback URL for GitHub */
 authRouter.get('/github/redirect', passport.authenticate('github'), AuthCtrl.oauth2);
+
+/* GETS the Facebook Login Screen */
+authRouter.get(
+  '/facebook',
+  passport.authenticate('facebook', {
+    scope: ['public_profile', 'email']
+  })
+);
+
+/* Callback URL for Facebook */
+authRouter.get(
+  '/facebook/redirect',
+  passport.authenticate('facebook', {
+    failureRedirect: '/login'
+  }),
+  AuthCtrl.oauth2
+);
 
 module.exports = authRouter;
