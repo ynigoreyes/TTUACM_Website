f4d0fd79287513425aee5273af2d62301cf2cb11
Miggy Reyes <ynigoreyes@gmail.com>
2018-06-05 12:05:30 -0500

    linted passport

--- config/passport.js
+++ config/passport.js
@@ -26,17 +26,19 @@ module.exports = (passport) => {
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
     secretOrKey: process.env.session_secret
   };
-  passport.use(new JwtStrategy(jwtOpts, (jwtPayload, done) => {
-    User.getUserById(jwtPayload.data._id, (err, user) => {
-      if (err) {
-        return done(err, false);
-      }
-      if (user) {
-        return done(null, user);
-      }
-      return done(null, false);
-    });
-  }));
+  passport.use(
+    new JwtStrategy(jwtOpts, (jwtPayload, done) => {
+      User.getUserById(jwtPayload.data._id, (err, user) => {
+        if (err) {
+          return done(err, false);
+        }
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false);
+      });
+    })
+  );
 
   // Google Strategy
   const googleClientID = process.env.google_clientID;
@@ -47,27 +49,29 @@ module.exports = (passport) => {
     clientID: googleClientID,
     clientSecret: googleClientSecret
   };
-  passport.use(new GoogleStrategy(googleOpts, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id }).then((currentUser) => {
-      if (currentUser) {
-        // User exists in database
-        done(null, currentUser);
-      } else {
-        // User does not exist... Create a new one
-        const data = {
-          googleId: profile.id,
-          email: profile.email,
-          firstName: profile.displayName.split(' ')[0],
-          lastName: profile.displayName.split(' ')[1],
-          verified: true
-        };
-        const newUser = new User(data);
-        newUser.save().then((user) => {
-          done(null, user);
-        });
-      }
-    });
-  }));
+  passport.use(
+    new GoogleStrategy(googleOpts, (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          // User exists in database
+          done(null, currentUser);
+        } else {
+          // User does not exist... Create a new one
+          const data = {
+            googleId: profile.id,
+            email: profile.email,
+            firstName: profile.displayName.split(' ')[0],
+            lastName: profile.displayName.split(' ')[1],
+            verified: true
+          };
+          const newUser = new User(data);
+          newUser.save().then((user) => {
+            done(null, user);
+          });
+        }
+      });
+    })
+  );
 
   // GitHub Strategy
   const githubClientID = process.env.github_clientID;
@@ -77,34 +81,32 @@ module.exports = (passport) => {
     clientID: githubClientID,
     clientSecret: githubClientSecret
   };
-  passport.use(new GitHubStrategy(githubOpts, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ githubId: profile.id })
-      .then((currentUser) => {
-        if (currentUser) {
-          done(null, currentUser);
-        } else {
-          // Sometimes, the user has their email access set to private
-          // In that case, we save their id instead
-          const emailData = profile._json.email === null
-            ? profile.id
-            : profile._json.email;
-          const data = {
-            githubId: profile.id,
-            email: emailData,
-            firstName: profile.displayName.split(' ')[0],
-            lastName: profile.displayName.split(' ')[1],
-            verified: true
-          };
-          const newUser = new User(data);
-          newUser.save()
-            .then((user) => {
+  passport.use(
+    new GitHubStrategy(githubOpts, (accessToken, refreshToken, profile, done) => {
+      User.findOne({ githubId: profile.id })
+        .then((currentUser) => {
+          if (currentUser) {
+            done(null, currentUser);
+          } else {
+            // Sometimes, the user has their email access set to private
+            // In that case, we save their id instead
+            const emailData = profile._json.email === null ? profile.id : profile._json.email;
+            const data = {
+              githubId: profile.id,
+              email: emailData,
+              firstName: profile.displayName.split(' ')[0],
+              lastName: profile.displayName.split(' ')[1],
+              verified: true
+            };
+            const newUser = new User(data);
+            newUser.save().then((user) => {
               done(null, user);
             });
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  })
-  )
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    })
+  );
 };
