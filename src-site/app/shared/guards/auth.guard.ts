3bb831beacaa8ba94160a8b87828b960c3f8a7ec
Miggy Reyes <ynigoreyes@gmail.com>
2018-06-10 13:58:27 -0500

    using canLoad for lazy loaded features

--- src-site/app/shared/guards/auth.guard.ts
+++ src-site/app/shared/guards/auth.guard.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router, CanActivate, CanLoad } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 import { UserStateService } from '../services/user-state.service';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(
     private router: Router,
     private snackBar: MatSnackBar,
     private userStateService: UserStateService
-  ) {
-
-  }
+  ) {}
 
   // Checks to see if the user is logged in. If not, it will redirect to Login
   canActivate() {
+    return this.checkLoggedIn();
+  }
+
+  canLoad() {
+    return this.checkLoggedIn();
+  }
+
+  private checkLoggedIn() {
     if (this.userStateService.loggedIn()) {
       return true;
     } else {
-      this.snackBar.open(`Unauthorized`, 'Close', { duration: 4000 });
+      this.snackBar.open(`You must login first`, 'Close', { duration: 4000 });
       this.router.navigate(['/login']);
     }
   }
