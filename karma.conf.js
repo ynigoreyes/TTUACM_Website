d8d36b1c9bfde2b22054388dff41561bc596bdd0
Miggy Reyes <ynigoreyes@gmail.com>
2018-06-16 04:19:16 -0500

    travis simplify

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,6 @@ module.exports = function(config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-phantomjs-launcher'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma'),
       require('@angular/material')
